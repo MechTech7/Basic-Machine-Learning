8a5c41607eaf7a65631f1e2a1a056bc64ab12db8
Mason <masontyler747@gmail.com>
2018-06-25 16:32:03 -0400

    still striving

--- backprop_obj_test.py
+++ backprop_obj_test.py
@@ -1,20 +1,3 @@
-import numpy as np
-import backprop_objects as bp
-
-'''x = np.array([[0,0,1], [2,2,1], [1,0,1], [1,1,1], [2,3,1]])
-y = np.array([[1],[1],[0],[1],[0]])'''
-
-pre_x = [[6.2,2.2,4.5,1.5],
-         [4.6,3.1,1.5,0.2],
-         [7.9,3.8,6.4,2.0]]
-pre_y = [[1],
-         [0],
-         [2]]
-x = np.array(pre_x)
-y = np.array(pre_y)
-
-net = bp.Network(x, y, layers=3)
-print(x)
-print(y)
-net.train(10000)
-print(net.activate_network(x[1]))
+version https://git-lfs.github.com/spec/v1
+oid sha256:3113162ab17b5b5ab261fcf90ccaa0f355379fcb5b0c890651b2c2b282509924
+size 422
